f5cd28a21cdaa5b9b828db12da4d99b27089ae4e
André Pires <andre.r.o.pires@gmail.com>
2015-10-21 00:39:31 +0100

    Added interoperability from example

---
+++ SINF/Lib_Primavera/Model/Artigo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SINF.Lib_Primavera.Model
+{
+    public class Artigo
+    {
+        public string CodArtigo
+        {
+            get;
+            set;
+        }
+
+        public string DescArtigo
+        {
+            get;
+            set;
+        }
+
+    }
+}
