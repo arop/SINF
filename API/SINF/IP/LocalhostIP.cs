7013c00ea65f5b1089d4ecb0165ff1fca6726fd5
André Pires <andre.r.o.pires@gmail.com>
2015-10-23 10:50:31 +0100

    added non hardcoded ip

---
+++ API/SINF/IP/LocalhostIP.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SINF.IP
+{
+    public class LocalhostIP : Controller
+    {
+        //
+        // GET: /LocalhostIP/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public static String localhostIP()
+        {
+            return "http://localhost:50664";
+        }
+
+    }
+}
