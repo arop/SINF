1a4022b4949e475bbd8a4e65aec4862614c56dd5
André Pires <andre.r.o.pires@gmail.com>
2015-11-16 15:54:47 +0000

    added table for images

--- site/db/schema.rb
+++ site/db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151115122738) do
+ActiveRecord::Schema.define(version: 20151116154943) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "imagems", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "image"
+    t.string   "idProduto"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
